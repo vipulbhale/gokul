e4902d808fa666974495de171a9bc4ac93a3a6c1
Vipul Bhale <3891937+vipulbhale@users.noreply.github.com>
2018-08-19 10:23:48 -0400

    Fix the issue related to the reflection.
    Fix the issue related to the New method in controllerregistry.go.

--- server/reflect/reflect.go
+++ server/reflect/reflect.go
@@ -42,14 +42,14 @@ func RegisterControllers() map[string]reflect.Value{
 	{{ end }}
 }
 
-func New(name string) (interface{}, bool) {
-	t, ok := mapOfControllerNameToControllerObj[name]
-	if !ok {
-		return nil, false
-	}
-	v := reflect.New(t)
-	return v.Interface(), true
-}
+// func New(name string) (interface{}, bool) {
+// 	t, ok := mapOfControllerNameToControllerObj[name]
+// 	if !ok {
+// 		return nil, false
+// 	}
+// 	v := reflect.New(t)
+// 	return v.Interface(), true
+// }
 `
 
 type controllerSpec struct {
